447990cd420f92c5ffaf481ac5e0c895de5c8502
avms <andrevmuzy@gmail.com>
2022-06-05 17:17:23 -0300

    list

--- list.py
+++ list.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import random
 lista = []
-for c in range(1, 100):
+for c in range(1, 10):
     lista = ([random.randint(1, 60) for x in range(6)])
     print(sorted(lista))
+df = pd.DataFrame(lista, columns = ['Numeros'])
+print(df)
