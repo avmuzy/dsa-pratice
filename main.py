aac425c95cd28f71bedd38d8839f6c814450237a
avms <andrevmuzy@gmail.com>
2022-07-23 19:26:06 -0300

    Game menu

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+print('=' * 40)
+print('Welcome to Hangman game')
+print('''Choose the language you want to play:
+[1] English
+[2] Português
+[3] Español''')
+print('=' * 40)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+option = int(input('Enter your language:'))
